acacec7d8f8601ad4fbbf61428919a9e0882ef81
Devin Halladay <devin@devinhalladay.com>
2021-11-06 21:04:40 -0400

    Remove initialData

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,8 @@
 import type { NextPage } from "next";
 import { KeyboardEvent, useState } from "react";
 import ReactFlow, {
-  removeElements,
-  addEdge,
-  useUpdateNodeInternals,
+  addEdge, Connection, Edge, Elements, removeElements
 } from "react-flow-renderer";
-
-import { FlowElement, Elements, Connection, Edge } from "react-flow-renderer";
-
-import { FlowNode } from "../types";
-
-const initialElements = [
-  // {
-  //   id: "1",
-  //   type: "input", // input node
-  //   data: { label: "Input Node" },
-  //   position: { x: 250, y: 25 },
-  // },
-  // // default node
-  // {
-  //   id: "2",
-  //   // you can also pass a React component as a label
-  //   data: { label: <div>Default Node</div> },
-  //   position: { x: 100, y: 125 },
-  // },
-  // {
-  //   id: "3",
-  //   type: "output", // output node
-  //   data: { label: "Output Node" },
-  //   position: { x: 250, y: 250 },
-  // },
-  // // animated edge
-  // // { id: "e1-2", source: "1", target: "2", animated: true },
-  // // { id: "e2-3", source: "2", target: "3" },
-];
 
 const Home: NextPage = () => {
   // store a list of elements and edges in state
